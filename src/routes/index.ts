3597abdd7b8011675fa98e92ef40868cc1511107
Eduardo-Baranowski <eduardobaranowski@gmail.com>
2021-05-07 16:57:31 -0300

    últimas configs

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,26 @@
-import { Router } from 'express';
+import { response, Router } from 'express';
+import axios from 'axios';
 
 const routes = Router();
 
-routes.get('/', (request, response) => {
+routes.get('/', async(request, response) => {
+    const token = '5bf4fbbbef517abe966a645646e109e5ca3764b0'
+
+    const data  = await axios.get('https://brasil.io/api/dataset/covid19/caso/data/?state=PR&date=2020-05-10', {
+        headers: {
+            'Authorization': `Token ${token}`,
+            'User-Agent': 'python-urllib/brasilio-client-0.1.0'
+          }
+    })
+    .then((res) => {
+        console.log(res.data)
+    })
+    .catch((error) => {
+        console.error(error)
+    });
+
+    console.log(data);
+
     return response.json({message: 'Hello World'})
 });
 
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
 import express, { request, response } from 'express';
-import routes from "./routes";
+import routes from './routes';
+import cors from 'cors';
 
 const app = express();
+
+app.use(routes);
+app.use(cors());
 
 app.listen(3333, () => {
     console.log('Server started on port 3333!')
